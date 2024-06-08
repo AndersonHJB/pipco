0d3c440c8ecbddaed3a935c7b78df33845e4b2bb
AndersonHJB <aiyuechuang@gmail.com>
2024-06-08 22:26:47 +0800

    Update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/AndersonHJB/pipco'
 EMAIL = 'aiyuechuang@gmail.com'
 AUTHOR = 'aiyuechuang'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
