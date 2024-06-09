3ae108221ff74ee1821d54b3b250022f4de78a7e
AndersonHJB <aiyuechuang@gmail.com>
2024-06-09 09:12:29 +0800

    添加个人二维码

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ pco help
 
 ## 个人二维码
 
-![img.png](img.png)
+![img.png](./images/img.png)
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/AndersonHJB/pipco'
 EMAIL = 'aiyuechuang@gmail.com'
 AUTHOR = 'aiyuechuang'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.11'
+VERSION = '0.0.12'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
